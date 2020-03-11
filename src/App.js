9f9b7abf77cc2d16435035e51f9ef93d6ea9e49f
sowmyy <sowmyanarayanan.s@tcs.com>
2020-03-11 12:39:41 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../src/styles/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>O365 ENGAGE</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
